c730c62e74efec76da6b0608625e57042d3fc107
lucides7 <chimsedicom5237@gmail.com>
2018-02-01 16:32:58 +0700

    tao chuong trinh pn.cpp

---
+++ pn.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+	int x =3,a,b;
+	if ( a < 0)
+	{
+		if ( b < 0)
+		{
+			x++;
+		}
+		else if ( b>0)
+		{
+			x--;
+		}
+		else 
+		{
+			x = x *2;
+		}
+	}
+	if (a < 5)
+	{
+		if (b < 5)
+		{
+			x--;
+		}
+		if ( b == 0)
+		{
+			x = x* 2;
+		}
+	}
+	else {
+		x = x * 2;
+	}
+}
